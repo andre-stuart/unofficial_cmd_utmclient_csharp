b80ad52a02e36db172346b14c3b48dbc1596078a
aoliveira <aoliveira@blockbit.com>
2020-09-23 10:04:15 -0300

    treatment and validation of arguments passed from command line

---
+++ utmauth/utmauth/Args.cs
@@ -0,0 +1,97 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security;
+
+namespace utmauth
+{
+    class Args
+    {
+        private readonly string[] _args;
+
+        public Args() { }
+        public Args(string[] args)
+        {
+            _args = args;
+            try { this.Parse(); Status = true; } catch { Status = false; };
+        }
+
+        public void Parse()
+        {
+            for (int i = 0; i < _args.Length; i++)
+            {
+                if (_args[i] == "--action")
+                    Action = _args[i++].Contains("--") ? "" : _args[i++].Trim().ToLower();
+
+                else if (_args[i] == "--server")
+                    Server = _args[i++].Contains("--") ? "" : _args[i++].Trim().ToLower();
+
+                else if (_args[i] == "--port")
+                    Port = _args[i++].Contains("--") ? "" : _args[i++].Trim();
+
+                else if (_args[i] == "--login")
+                    Login = _args[i++].Contains("--") ? "" : _args[i++].Trim().ToLower();
+
+                else if (_args[i] == "--pass")
+                    Pass = _args[i++].Contains("--") ? new NetworkCredential("", "").SecurePassword : new NetworkCredential("", _args[i++].Trim()).SecurePassword;
+
+                else if (_args[i] == "--logfile")
+                    LogFile = _args[i++].Contains("--") ? "" : _args[i++].Trim();
+
+                else if (_args[i] == "--cookie")
+                    CookieFile = _args[i++].Contains("--") ? "" : _args[i++].Trim();
+
+                else if (_args[i] == "--keepalive")
+                    KeepaliveTime = _args[i++].Contains("--") ? "" : _args[i++].Trim();
+            }
+
+            if (Server.Length < 1 || Login.Length < 1 || Pass.Length < 1)
+                throw new System.ArgumentException("Parameter invalid", "");
+
+            Validate v = new Validate();
+
+            if (Action.Length < 1)
+                Action = "login";
+            else if (!v.IsAction(Action))
+                throw new System.ArgumentException("Parameter invalid", "action");
+
+            if (Port.Length < 1)
+                Port = "9803";
+            else if (!v.IsPort(Port))
+                throw new System.ArgumentException("Parameter invalid", "port");
+
+            if (LogFile.Length < 1)
+                LogFile = "utmauth.log";
+            else if (!v.IsLogFile(LogFile))
+                throw new System.ArgumentException("Parameter invalid", "logfile");
+
+            if (CookieFile.Length < 1)
+                CookieFile = "utmauth.cookie";
+            else if (!v.IsCookieFile(LogFile))
+                throw new System.ArgumentException("Parameter invalid", "cookie");
+
+            if (KeepaliveTime.Length < 1)
+                KeepaliveTime = "0";
+            else if (!v.IsKeepaliveTime(LogFile))
+                throw new System.ArgumentException("Parameter invalid", "keepalive");
+        }
+
+        public bool Status { get; }
+
+        [StringLength(20)]
+        public string Action { get; set; }
+        [StringLength(253)]
+        public string Server { get; set; }
+        [StringLength(5)]
+        public string Port { get; set; }
+        [StringLength(320)]
+        public string Login { get; set; }
+        [StringLength(320)]
+        public SecureString Pass { get; set; }
+        [StringLength(4)]
+        public string KeepaliveTime { get; set; }
+        [StringLength(1024)]
+        public string LogFile { get; set; }
+        [StringLength(1024)]
+        public string CookieFile { get; set; }
+    }
+}
---
+++ utmauth/utmauth/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace utmauth
+{
+    class Program
+    {
+        struct Status
+        {
+            public const int SUCCESS = 0;
+            public const int ERR_UNKNOW = -1;
+            public const int ERR_PARAM = -2;
+            public const int ERR_CONN = -3;
+            public const int ERR_AUTH = -4;
+        };
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 || args.Any(x => x.Contains("--help")))
+            {
+                Help();
+                Environment.Exit(Status.ERR_PARAM);
+            }
+
+            var opt = new Args(args);
+            if (!opt.Status)
+            {
+                Help();
+                Environment.Exit(Status.ERR_PARAM);
+            }
+
+            //Console.WriteLine("OKOK");
+            Environment.Exit(Status.SUCCESS);
+        }
+
+        static void Help()
+        {
+            Console.WriteLine("unofficial client for authentication without interface in Blockbit® utm client");
+            Console.WriteLine("");
+            Console.WriteLine("Using:");
+            Console.WriteLine("  utmauth.exe --action[ACTION] --server[ip/host] --login[login/email] --pass[password]");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine("  --help                -> Show this help message.");
+            Console.WriteLine("  --action[ACTION]      -> [Optional] login, logout, keepalive or status [default: login]. ex: login");
+            Console.WriteLine("  --server[ip/host]     -> [Required] UTM server ip or host. ex: utm.exmaple.com");
+            Console.WriteLine("  --port[port]          -> [Optional] UTM server port [default: 9803]. ex: 9803");
+            Console.WriteLine("  --login[login/email]  -> [Required] UTM login or login@domain. ex: user@exmaple.com");
+            Console.WriteLine("  --pass[password]      -> [Required] UTM user password.");
+            Console.WriteLine("  --logfile[uri]        -> [Optional] Uri log file [default: ./utmauth.log]. ex: c:\\utmauth.log");
+            Console.WriteLine("  --cookie[uri]         -> [Optional] Uri cookie file [default: ./utmauth.cookie]. ex: c:\\utmauth.cookie");
+            Console.WriteLine("  --keepalive[interval] -> [Optional] Keepalive interval in seconds limited 3600 [default: 0]. ex: 30");
+            Console.WriteLine("    *if none keepalive, empty or equal to 0(zero) keepalive disabled");
+            Console.WriteLine("");
+            Console.WriteLine("Example:");
+            Console.WriteLine("  > utmauth.exe --action login --server utm.exmaple.com --login user@exmaple.com --pass \"*******\"");
+            Console.WriteLine("  > utmauth.exe --action logout --server utm.exmaple.com --login user@exmaple.com");
+            Console.WriteLine("");
+            Console.WriteLine("Developer by Andre Stuart [nbbr.andre@gmail.com]");
+            Console.WriteLine("");
+            Console.WriteLine("Blockbit® is a registered trademark of BLOCKBIT TECNOLOGIA LTDA [https://www.blockbit.com/about/]");
+        }
+    }
+}
